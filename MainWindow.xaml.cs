9d5e684d539f4e092220db3e16483776ad4b4e24
ilj13253 <butackovilj25@gmail.com>
2024-02-26 23:34:39 +0800

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp18.ViewModel;
+namespace WpfApp18
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext =new AppViewModel();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(countword);
+            t.IsBackground = true;
+            t.Start(FileName);
+        }
+        void countword(object data)
+        {
+            var path = data as string;
+            var text = File.ReadAllText(@"C:\Users\Илья\source\repos\WpfApp17\obj\Debug\Текстовый документ.txt");
+            var worldls = text.Split(' ');
+            //MessageBox.Show(worldls.Length.ToString());
+            Dispatcher.Invoke(() => { Result.Content = "Result:" + worldls.Length.ToString(); });
+        }
+    }
+}
---
+++ ViewModel/AppViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfApp18.ViewModel
+{
+    class AppViewModel : ViewModelBase
+    {
+        private string fileName = @"C:\Users\Илья\source\repos\WpfApp18\obj\Debug\Текстовый документ.txt";
+        public string FileName {
+            get { return fileName; }
+            set { Set(ref fileName, value); }
+        }
+        private string result;
+        public string Result
+        {
+            get { return result; }
+            set { Set(ref result, value); }
+        }
+        private RelayCommand startCommand;
+        public RelayCommand StartCommand{
+            get => startCommand ?? (startCommand = new RelayCommand(() => { Thread t = new Thread(countword);t.Start(FileName); }));
+        }
+        void countword(object data)
+        {
+            var path = data as string;
+            var text = File.ReadAllText(path);
+            var worldls = text.Split(' ');
+            //MessageBox.Show(worldls.Length.ToString());
+            Result = "Result:" + worldls.Length.ToString();
+        }
+    }
+}
